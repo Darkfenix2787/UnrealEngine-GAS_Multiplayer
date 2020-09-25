a7e75970766519d994a39ccf1223a6dfa58e19be
darkfenix2787 <danielcorredorm@gmail.com>
2020-09-24 22:13:05 -0500

    AttributeSet

--- Source/ArkdeCM/Private/ACM_AttributeSet.cpp
+++ Source/ArkdeCM/Private/ACM_AttributeSet.cpp
@@ -2,4 +2,75 @@
 
 
 #include "ACM_AttributeSet.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
 
+//===========================================================================================================================================================
+UACM_AttributeSet::UACM_AttributeSet() 
+{
+	MaxHealth = 100.f;
+	Health = MaxHealth;
+	HealthRegen = 0.5f;
+
+	MaxMana = 100.f;
+	Mana = MaxMana;
+	ManaRegen = 2.f;
+
+	MaxStamina = 100.f;
+	Stamina = MaxStamina;
+	StaminaRegen = 1.f;
+}
+
+//===========================================================================================================================================================
+void UACM_AttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		AdjustAttributeForMaxChange(Health, MaxHealth, NewValue, GetHealthAttribute());
+	}
+	else if (Attribute == GetMaxManaAttribute())
+	{
+		AdjustAttributeForMaxChange(Mana, MaxMana, NewValue, GetManaAttribute());
+	}
+	else if (Attribute == GetMaxStaminaAttribute())
+	{
+		AdjustAttributeForMaxChange(Stamina, MaxStamina, NewValue, GetStaminaAttribute());
+	}
+	
+}
+
+//===========================================================================================================================================================
+void UACM_AttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	if (Data.EvaluatedData.Attribute.GetUProperty() == FindFieldChecked<FProperty>(UACM_AttributeSet::StaticClass(), GET_MEMBER_NAME_CHECKED(UACM_AttributeSet, Health)))
+	{
+		Health.SetCurrentValue(FMath::Clamp(Health.GetCurrentValue(), 0.f, MaxHealth.GetCurrentValue()));
+		Health.SetBaseValue(FMath::Clamp(Health.GetBaseValue(), 0.f, MaxHealth.GetCurrentValue()));
+	}
+	else if (Data.EvaluatedData.Attribute.GetUProperty() == FindFieldChecked<FProperty>(UACM_AttributeSet::StaticClass(), GET_MEMBER_NAME_CHECKED(UACM_AttributeSet, Mana)))
+	{
+		Mana.SetCurrentValue(FMath::Clamp(Mana.GetCurrentValue(), 0.f, MaxMana.GetCurrentValue()));
+		Mana.SetBaseValue(FMath::Clamp(Mana.GetBaseValue(), 0.f, MaxMana.GetCurrentValue()));
+	}
+	else if (Data.EvaluatedData.Attribute.GetUProperty() == FindFieldChecked<FProperty>(UACM_AttributeSet::StaticClass(), GET_MEMBER_NAME_CHECKED(UACM_AttributeSet, Stamina)))
+	{
+		Stamina.SetCurrentValue(FMath::Clamp(Stamina.GetCurrentValue(), 0.f, MaxStamina.GetCurrentValue()));
+		Stamina.SetBaseValue(FMath::Clamp(Stamina.GetBaseValue(), 0.f, MaxStamina.GetCurrentValue()));
+	}
+}
+
+//===========================================================================================================================================================
+void UACM_AttributeSet::AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAtribute, const FGameplayAttributeData& MaxAtribute, float NewMaxValue, const FGameplayAttribute& AffectedAtributeProperty)
+{
+	UAbilitySystemComponent* AbilityComponent = GetOwningAbilitySystemComponent();
+	const float CurrentMaxValue = MaxAtribute.GetCurrentValue();
+	if (!FMath::IsNearlyEqual(CurrentMaxValue, NewMaxValue) && IsValid(AbilityComponent))
+	{
+		const float CurrentValue = AffectedAtribute.GetCurrentValue();
+		float NewDelta = CurrentMaxValue > 0.f ? (CurrentValue * NewMaxValue / CurrentMaxValue) - CurrentValue : NewMaxValue;
+
+		AbilityComponent->ApplyModToAttributeUnsafe(AffectedAtributeProperty, EGameplayModOp::Additive, NewDelta);
+	}
+}
--- Source/ArkdeCM/Public/ACM_AttributeSet.h
+++ Source/ArkdeCM/Public/ACM_AttributeSet.h
@@ -4,14 +4,73 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "ACM_AttributeSet.generated.h"
 
-/**
- * 
- */
+// Uses macros from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class ARKDECM_API UACM_AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+
+	//Functions
+	UACM_AttributeSet();
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	void AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAtribute, const FGameplayAttributeData& MaxAtribute, float NewMaxValue, const FGameplayAttribute& AffectedAtributeProperty);
+
+public:
+
+	//Attributes
+
+	//Health
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+		FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+		FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+		FGameplayAttributeData HealthRegen;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, HealthRegen);
+
+	//Mana
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+		FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, Mana);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+		FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, MaxMana);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+		FGameplayAttributeData ManaRegen;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, ManaRegen);
+
+	//Mana
+	UPROPERTY(BlueprintReadOnly, Category = "Stamina")
+		FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, Stamina);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stamina")
+		FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, MaxStamina);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Stamina")
+		FGameplayAttributeData StaminaRegen;
+	ATTRIBUTE_ACCESSORS(UACM_AttributeSet, StaminaRegen);
+
+
 	
 };
